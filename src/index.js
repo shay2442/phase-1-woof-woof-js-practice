b0d5f9854be8e94eb06bb0829a63d3aca10ed499
Shay Wacker <shay2442@yahoo.com>
2021-09-09 13:08:33 -0500

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,6 +1,25 @@
 let dogsArray = []
+let filtering = false
+
 const dogBar = document.querySelector("#dog-bar")
 const detailDiv = document.querySelector('#dog-info')
+const filterButton = document.querySelector("#good-dog-filter")
+
+filterButton.addEventListener('click', () => {
+filtering = !filtering
+filterButton.textContext =`Filter good dogs: ${filtering ? 'ON' : 'OFF'}`
+
+if (filtering)  {
+    const goodDogsOnly = dogsArray.filter(dog => dog.isGoodDog)
+    dogBar.innerHTML = ''
+    renderAllDogs(goodDogsOnly)
+
+} else {
+    dogBar.innerHTML = ""
+    renderAllDogs(dogsArray)
+}
+
+})
 
 
 dogBar.addEventListener('click', e => {
@@ -17,6 +36,23 @@ detailDiv.addEventListener('click', e => {
         const dogId = parseInt(e.target.dataset.id)
         const foundDog = dogsArray.find(dog => dog.id === dogId)
         foundDog.isGoodDog = !foundDog.isGoodDog
+        renderDogDetail(foundDog)
+
+
+fetch(`http://localhost:3000/pups${dogId}`), {
+    method: 'PATCH',
+    headers: {
+        "Content-Type": "application/json"
+    },
+    body: JSON.stringify( {isGoodDog: foundDog.isGoodDog })
+}
+
+
+
+
+
+
+
     }
 })
 
@@ -28,7 +64,7 @@ function renderDogDetail(dog) {
     detailDiv.innerHTML = `
     <img src=${dog.image}>
     <h2>${dog.name}</h2>
-    <button>${status}!</button>
+    <button data-id=${dog.id}>${status}!</button>
     `
 }
 
